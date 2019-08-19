e9f5926de19575eaecd55ee19535e04697577f11
Scott-GH <thatguywhowas@live.com>
2019-08-18 17:35:02 -0700

    Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,27 +6,19 @@ class Counter extends Component {
         tags: ['tag1','tag2','tag3']
     };
 
-    render() {        
-        return (
-            <React.Fragment>                    
-                <span className={this.getCounterClasses()}>
-                    {this.formatCount()}
-                </span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul>
-            </React.Fragment>);
-    }
-    getCounterClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0 ? "warning" : "primary");
-        return classes;
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>There are no tags</p>;
+
+        return <ul>
+            {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+        </ul>;
     }
 
-    formatCount(){
-        const {count} = this.state;
-        return count === 0 ? "Zero" : count;
+    render() {        
+        return (
+            <React.Fragment>                                    
+                {this.renderTags()}    
+            </React.Fragment>);
     }
 }
  

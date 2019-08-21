4bb426cac2a463ed1b5bb3bcf2620281569631c4
Scott-GH <thatguywhowas@live.com>
2019-08-21 16:19:28 -0700

    Passing Event Params Sol 2

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -20,17 +20,13 @@ class Counter extends Component {
         this.setState({count: this.state.count + 1});
     };
 
-    doHandleIncrement = () =>{
-        this.handleIncrement({id: 1})
-    };
-
     render() {     
         return (
             <React.Fragment>                    
                 <span className="badge badge-primary m-2">
                     {this.formatCount()}
                 </span>
-                <button onClick={this.doHandleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.handleIncrement({id: 1})} className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>);
     }
     formatCount(){

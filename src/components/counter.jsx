df22c2ee66efdaaa4ff509425e0e032e5060defe
Scott-GH <thatguywhowas@live.com>
2019-08-21 16:24:20 -0700

    Implemented Counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -22,12 +22,12 @@ class Counter extends Component {
 
     render() {     
         return (
-            <React.Fragment>                    
+            <div>                    
                 <span className="badge badge-primary m-2">
                     {this.formatCount()}
                 </span>
                 <button onClick={() => this.handleIncrement({id: 1})} className="btn btn-secondary btn-sm">Increment</button>
-            </React.Fragment>);
+            </div>);
     }
     formatCount(){
         const {count} = this.state;

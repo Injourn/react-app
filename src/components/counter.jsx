b020a2564acea789b1fd9507bbf1dabba6d60121
Scott-GH <thatguywhowas@live.com>
2019-08-23 14:39:03 -0700

    Updated counter to a controlled component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        value: this.props.counter.value
-    };
+    
 
     /*styles = {
         fontSize: 50,
@@ -15,11 +13,6 @@ class Counter extends Component {
         this.handleIncrement = this.handleIncrement.bind(this);
     }*/
 
-    handleIncrement = product =>{
-        console.log(product);
-        this.setState({value: this.state.value + 1});
-    };
-
     render() {     
         return (
             <div>       
@@ -27,12 +20,12 @@ class Counter extends Component {
                 <span className="badge badge-primary m-2">
                     {this.formatCount()}
                 </span>
-                <button onClick={() => this.handleIncrement({id: 1})} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
                 <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>);
     }
     formatCount(){
-        const {value: count} = this.state;
+        const {value: count} = this.props.counter;
         return count === 0 ? "Zero" : count;
     }
 }
